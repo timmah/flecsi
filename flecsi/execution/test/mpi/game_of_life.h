a23c7b634a60e6aa6a73f43cdf6f1bb4b6b706c6
Li-Ta Lo <lita.ollie@gmail.com>
2017-02-21 13:53:38 -0700

    extract stencil operation into its own task

--- flecsi/execution/test/mpi/game_of_life.h
+++ flecsi/execution/test/mpi/game_of_life.h
@@ -12,6 +12,51 @@
 #include "flecsi/partition/weaver.h"
 #include "flecsi/topology/index_space.h"
 #include "flecsi/topology/graph_utils.h"
+#include "flecsi/execution/execution.h"
+
+void stencil_task(flecsi::io::simple_definition_t& sd, std::set<size_t>& primary_cells,
+                  std::unordered_map<size_t, int>& alive,
+                  std::unordered_map<size_t, int>& alive2)
+{
+  // This should be replace with some kind of "task"
+  for (auto cell : primary_cells) {
+    std::set <size_t> me{cell};
+    // FIXME: connectivity information should be part of distributed data as well.
+    auto closure = flecsi::topology::entity_closure<2, 2, 0>(sd, me);
+    auto nearest_neighbors = flecsi::utils::set_difference(closure, me);
+
+    int count = 0;
+    for (auto neighbor : nearest_neighbors) {
+      if (neighbor != -1 && alive[neighbor] == 1) {
+        count++;
+      }
+    }
+    if (alive[cell]) {
+      if (count < 2)
+        alive2[cell] = 0;
+      else if (count <= 3)
+        alive2[cell] = 1;
+      else
+        alive2[cell] = 0;
+    } else {
+      if (count == 3)
+        alive2[cell] = 1;
+    }
+  }
+
+  for (auto cell : primary_cells) {
+    if (alive2[cell] == 1) {
+      std::cout << "primary cell: " << cell
+                << ", alive: " << alive2[cell]
+                << std::endl;
+    }
+  }
+  std::cout << std::endl;
+
+  std::swap(alive, alive2);
+}
+
+register_task(stencil_task, loc, single);
 
 void driver(int argc, char **argv) {
   int rank;
@@ -38,13 +83,13 @@ void driver(int argc, char **argv) {
 
   // initialize the center 3 cells to be alive (a row), it is a period 2 blinker
   // going horizontal and then vertical.
-  if (alive.count(27) != 0) {
+  if (primary_cells.count(27) != 0) {
     alive[27] = 1;
   }
-  if (alive.count(35) != 0) {
+  if (primary_cells.count(35) != 0) {
     alive[35] = 1;
   }
-  if (alive.count(43) != 0) {
+  if (primary_cells.count(43) != 0) {
     alive[43] = 1;
   }
 
@@ -55,7 +100,7 @@ void driver(int argc, char **argv) {
                 << std::endl;
     }
   }
-  std::set <entry_info_t> exclusive_cells = weaver.get_exclusive_cells();
+
   std::set <entry_info_t> shared_cells = weaver.get_shared_cells();
   std::set <entry_info_t> ghost_cells = weaver.get_ghost_cells();
 
@@ -68,7 +113,7 @@ void driver(int argc, char **argv) {
   auto alive2 = alive;
 
   for (auto i = 0; i < 5; i++) {
-    // Do halo exchange first
+    // Do halo exchange first, replace with some kind of "halo exchange" task.
     std::vector <MPI_Request> requests;
     // Post receive for ghost cells
     for (auto ghost : ghost_cells) {
@@ -77,9 +122,9 @@ void driver(int argc, char **argv) {
                 ghost.rank, 0, MPI_COMM_WORLD, &requests[requests.size() - 1]);
     }
     // Send shared cells
-    for (auto entry : shared_cells) {
-      for (auto dest: entry.shared) {
-        MPI_Send(&alive[entry.id], 1, MPI_INT,
+    for (auto shared : shared_cells) {
+      for (auto dest: shared.shared) {
+        MPI_Send(&alive[shared.id], 1, MPI_INT,
                  dest, 0, MPI_COMM_WORLD);
       }
     }
@@ -87,42 +132,7 @@ void driver(int argc, char **argv) {
     std::vector <MPI_Status> status(requests.size());
     MPI_Waitall(requests.size(), &requests[0], &status[0]);
 
-    // This should be replace with some kind of "task"
-    for (auto cell : primary_cells) {
-      std::set <size_t> me{cell};
-      // FIXME: connectivity information should be part of distributed data as well.
-      auto closure = flecsi::topology::entity_closure<2, 2, 0>(sd, me);
-      auto nearest_neighbors = flecsi::utils::set_difference(closure, me);
-
-      int count = 0;
-      for (auto neighbor : nearest_neighbors) {
-        if (neighbor != -1 && alive[neighbor] == 1) {
-          count++;
-        }
-      }
-      if (alive[cell]) {
-        if (count < 2)
-          alive2[cell] = 0;
-        else if (count <= 3)
-          alive2[cell] = 1;
-        else
-          alive2[cell] = 0;
-      } else {
-        if (count == 3)
-          alive2[cell] = 1;
-      }
-    }
-
-    for (auto cell : primary_cells) {
-      if (alive2[cell] == 1) {
-        std::cout << "primary cell: " << cell
-                  << ", alive: " << alive2[cell]
-                  << std::endl;
-      }
-    }
-    std::cout << std::endl;
-
-    std::swap(alive, alive2);
+    execute_task(stencil_task, loc, single, sd, primary_cells, alive, alive2);
   }
 } // driver
 
